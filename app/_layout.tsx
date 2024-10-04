ed2dbd352c0df10d514c472d0b7466a3e3875269
Victor Chamalé <chamale.victor@gmail.com>
2024-10-03 22:51:10 -0600

    feat: first routes (#1)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@hooks/useColorScheme';
-import { Provider } from 'react-redux';
-import store from 'store/configureStore';
-import { ActivityIndicator, View } from 'react-native';
-import checkToken from 'utils/checkToken';
+import { useColorScheme } from "@hooks/useColorScheme";
+import { Provider } from "react-redux";
+import store from "store/configureStore";
+import { ActivityIndicator, View } from "react-native";
+import checkToken from "utils/checkToken";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-  const [initialRoute, setInitialRoute] = useState<'home' | 'sign-in' | null>(null);
+  const [initialRoute, setInitialRoute] = useState<"home" | "sign-in" | null>(
+    null
+  );
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -30,7 +36,7 @@ const RootLayout = () => {
   useEffect(() => {
     const initialize = async () => {
       const isLoggedIn = await checkToken();
-      setInitialRoute(isLoggedIn ? 'home' : 'sign-in');
+      setInitialRoute(isLoggedIn ? "home" : "sign-in");
     };
 
     initialize();
@@ -42,17 +48,19 @@ const RootLayout = () => {
 
   if (!initialRoute) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" />
       </View>
     );
   }
 
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Provider store={store}>
-        <Stack initialRouteName={initialRoute ?? 'sign-in'} screenOptions={_ => ({ headerShown: false })}>
+        <Stack
+          initialRouteName={initialRoute ?? "sign-in"}
+          screenOptions={(_) => ({ headerShown: false })}
+        >
           <Stack.Screen name="sign-in" />
           <Stack.Screen name="home" />
           <Stack.Screen name="+not-found" />
@@ -60,6 +68,6 @@ const RootLayout = () => {
       </Provider>
     </ThemeProvider>
   );
-}
+};
 
 export default RootLayout;
--- app/home.tsx
+++ app/home.tsx
@@ -5,124 +5,162 @@ import { Pressable, SafeAreaView, Text, TextInput, View } from "react-native";
 
 type HomePropsT = {
   navigation: any;
-}
+};
 
 const Home: FC<HomePropsT> = ({ navigation }) => {
-
   // hooks
   const router = useRouter();
 
-  return <SafeAreaView style={{ flex: 1 }}>
-    <View style={{ flex: 1, padding: 15 }}>
-      <Space vertical size={10} />
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-      }}>
-        <View></View>
-        <View style={{
-          marginRight: 10
-        }}>
-          <Pressable onPress={() => {
-            router.push('/sign-in');
-          }}>
-            <Text style={{
-              color: '#5db075',
-              fontSize: 20
-            }}>Salir</Text>
-          </Pressable>
-        </View>
-      </View>
-      <Space vertical size={10} />
-      <View
-        style={{
-          width: '100%',
-          borderColor: '#ddd9d9eb',
-          backgroundColor: '#e7e7e7c2',
-          borderWidth: 1.5,
-          borderRadius: 25
-        }}>
-        <TextInput
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 15 }}>
+        <Space vertical size={10} />
+        <View
           style={{
-            color: '#2c4778',
-            fontWeight: '600',
-            fontSize: 15,
-            borderColor: '#fff',
-            borderWidth: 0,
-            padding: 10,
-            paddingRight: 0
+            flexDirection: "row",
+            justifyContent: "space-between",
           }}
-          editable={true}
-          onChangeText={val => { }}
-          value={''}
-          placeholder="Buscar"
-          placeholderTextColor="#989393"
-          keyboardType="default"
-        />
-      </View>
-      <Space vertical size={50} />
-      <View style={{
-        flexDirection: 'row'
-      }}>
-        <View style={{
-          width: '45%',
-          marginLeft: 10,
-          marginRight: 5
-        }}>
-          <Pressable onPress={() => {
-            router.push('/travel')
-          }}>
-            <View style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-            }}>
-              <View style={{
-                flexDirection: 'column'
-              }}>
-                <View style={{
-                  backgroundColor: '#5db075',
-                  padding: 60,
-                  borderRadius: 8
-                }}>
-                  <Text>img</Text>
-                </View>
-                <Space vertical size={10} />
-                <Text style={{ textAlign: 'center', color: '#5db075' }}>Viajes</Text>
-              </View>
-            </View>
-          </Pressable>
+        >
+          <View></View>
+          <View
+            style={{
+              marginRight: 10,
+            }}
+          >
+            <Pressable
+              onPress={() => {
+                router.push("./login/sign-in");
+              }}
+            >
+              <Text
+                style={{
+                  color: "#5db075",
+                  fontSize: 20,
+                }}
+              >
+                Salir
+              </Text>
+            </Pressable>
+          </View>
         </View>
-        <View style={{
-          width: '45%',
-          marginLeft: 10,
-          marginRight: 5
-        }}>
-          <Pressable onPress={() => {
-            router.push('/customer')
-          }}>
-            <View style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-            }}>
-              <View style={{
-                flexDirection: 'column'
-              }}>
-                <View style={{
-                  backgroundColor: '#5db075',
-                  padding: 60,
-                  borderRadius: 8
-                }}>
-                  <Text>img</Text>
+        <Space vertical size={10} />
+        <View
+          style={{
+            width: "100%",
+            borderColor: "#ddd9d9eb",
+            backgroundColor: "#e7e7e7c2",
+            borderWidth: 1.5,
+            borderRadius: 25,
+          }}
+        >
+          <TextInput
+            style={{
+              color: "#2c4778",
+              fontWeight: "600",
+              fontSize: 15,
+              borderColor: "#fff",
+              borderWidth: 0,
+              padding: 10,
+              paddingRight: 0,
+            }}
+            editable={true}
+            onChangeText={(val) => {}}
+            value={""}
+            placeholder="Buscar"
+            placeholderTextColor="#989393"
+            keyboardType="default"
+          />
+        </View>
+        <Space vertical size={50} />
+        <View
+          style={{
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              width: "45%",
+              marginLeft: 10,
+              marginRight: 5,
+            }}
+          >
+            <Pressable
+              onPress={() => {
+                router.push("./shipments/shipment");
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "column",
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "#5db075",
+                      padding: 60,
+                      borderRadius: 8,
+                    }}
+                  >
+                    <Text>img</Text>
+                  </View>
+                  <Space vertical size={10} />
+                  <Text style={{ textAlign: "center", color: "#5db075" }}>
+                    Viajes
+                  </Text>
                 </View>
-                <Space vertical size={10} />
-                <Text style={{ textAlign: 'center', color: '#5db075' }}>Clientes</Text>
               </View>
-            </View>
-          </Pressable>
+            </Pressable>
+          </View>
+          <View
+            style={{
+              width: "45%",
+              marginLeft: 10,
+              marginRight: 5,
+            }}
+          >
+            <Pressable
+              onPress={() => {
+                router.push("./clients/customer");
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "column",
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "#5db075",
+                      padding: 60,
+                      borderRadius: 8,
+                    }}
+                  >
+                    <Text>img</Text>
+                  </View>
+                  <Space vertical size={10} />
+                  <Text style={{ textAlign: "center", color: "#5db075" }}>
+                    Clientes
+                  </Text>
+                </View>
+              </View>
+            </Pressable>
+          </View>
         </View>
       </View>
-    </View>
-  </SafeAreaView>
+    </SafeAreaView>
+  );
 };
 
-export default Home
+export default Home;
