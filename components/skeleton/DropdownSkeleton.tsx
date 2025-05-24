b57d7c3d5159654514772f91bfa12aaf79dd6cdd
Kevin Mendez <kevinmendez@Kevins-MacBook-Pro.local>
2025-05-23 22:37:29 -0600

    fix: fixes

--- components/skeleton/DropdownSkeleton.tsx
+++ components/skeleton/DropdownSkeleton.tsx
@@ -1,13 +1,13 @@
 import { View } from 'react-native';
-import SkeletonLoading from 'expo-skeleton-loading'
+// import SkeletonLoading from 'expo-skeleton-loading'
 
 const DropdownSkeleton = () => {
   return (
-    <SkeletonLoading background={"#adadad"} highlight={"#ffffff"}>
+    // <SkeletonLoading background={"#adadad"} highlight={"#ffffff"}>
       <View style={{ flex:1 }}>
           <View style={{ backgroundColor: "#adadad", width: "100%", height: 40, borderRadius: 5 }} />
       </View>
-    </SkeletonLoading>
+    // </SkeletonLoading>
   );
 }
 
